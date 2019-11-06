4d6c69b193f1651cd0b0fdacc78b91abf48e419e
Nuño Sempere <nuno.sempere@gmail.com>
2019-11-06 19:45:09 +0100

    Add files via upload

---
+++ miniserver3.js
@@ -0,0 +1,70 @@
+const {createServer} = require("http");
+const fs = require('fs');
+const qs = require('querystring');
+
+let server = createServer((request, response) => {
+    
+    buttonNames = ["diary", "calib", "system", "emotionalMaturity", "listOfProjects", "ABTesting"];
+    for(let i=0; i<buttonNames.length; i++){
+        requestUrlRead = "/"+buttonNames[i]+"Read"
+        requestUrlWrite = "/"+buttonNames[i]+"Write"
+        //console.log("RequestUrlWrite =", requestUrlWrite);
+        requestFile = "/home/nuno/Documents/WEBDEV/JavaScript/Radamantis/textFiles/"+buttonNames[i]+".txt"
+        //console.log(requestFile);
+        if(request.url===requestUrlRead){
+            //console.log("DiaryRead");
+            
+            fs.readFile(requestFile, function(err, data) {
+                response.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"});
+                if(err){
+                    throw err;
+                } 
+                //console.log("SR");
+                response.write(data);
+                response.end();
+
+            });
+
+        }
+
+    }
+    
+    if(request.method == "POST"){
+        console.log("Request.url =", request.url);
+        console.log(request.method);
+        //console.log(request.method == "POST");            
+        
+            let body = '';
+            request.on('data', chunk => {
+                body += chunk.toString(); // convert Buffer to string
+            });
+            request.on('end', () => {
+                var post = qs.parse(body);
+                //console.log(post);
+                //console.log("post.fileName =\n", post.fileName);
+                //console.log("post.fileContents =\n", post.fileContents);
+
+                fs.writeFile("/home/nuno/Documents/WEBDEV/JavaScript/Radamantis/textFiles/"+post.fileName, post.fileContents, (err)=>{ 
+                    if( err ) { 
+                        throw err; 
+                    }
+                    console.log("SW");
+                }); 
+
+                
+
+                response.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"});
+                response.write('ok');
+                response.end();
+            });
+        
+    }
+    
+    
+});
+
+server.listen(8000);
+console.log("Listening! (port 8000)");
+
+
+// Fuck it, create something else just to answer POST requests
